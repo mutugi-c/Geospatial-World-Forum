9f6c23c45c745247565d238e7b48575a79760ba3
mutugi-c <mutugichris@gmail.com>
2023-03-13 23:51:02 +0100

    Rectify linter errors

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav__menu');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
 navMenu.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-})
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+});
