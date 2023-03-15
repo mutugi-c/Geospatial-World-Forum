afba7177902207f2f178e8d8e339d43e69473793
mutugi-c <mutugichris@gmail.com>
2023-03-15 18:02:08 +0100

    Add speakers section dynamically with JS

---
+++ js/speakers.js
@@ -0,0 +1,19 @@
+import speakersInfo from "./speakersInfo.js";
+
+document.querySelector('#speakers__container').innerHTML = speakersInfo.map(
+  (info) => `
+    <div class="speaker">
+    <img src=${info.Image} alt="Speaker's face" class="speaker__img" />
+    <div class="speaker__body">
+      <h4 class="speaker__name">${info.name}</h4>
+      <span class="speaker__summary">
+        ${info.summary}
+      </span>
+      <span class="bar--primary"></span>
+      <p class="speaker__details">
+        ${info.details}
+      </p>
+    </div>
+  </div> `,
+)
+  .join('');
--- js/speakersInfo.js
+++ js/speakersInfo.js
@@ -1,44 +1,46 @@
 const speakersInfo = [
-    {
-    name: 'Yochai Benkler',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-    },
+	{
+		name: 'Yochai Benkler',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
 
-{
-    name: 'Kilnam Chon',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-},
+	{
+		name: 'Kilnam Chon',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
 
-    {
-    name: 'SohYeong Noh',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-    },
+	{
+		name: 'SohYeong Noh',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
 
-    {
-    name: 'Julia Leda',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-    },
-    {
-    name: 'Lila tretikov',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-        },
-        {
-    name: 'Ryan Merkley',
-    Image: './images/speaker.png',
-    summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
-    details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
-    },
+	{
+		name: 'Julia Leda',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
 
-]  
+	{
+		name: 'Lila tretikov',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
+	
+	{
+		name: 'Ryan Merkley',
+		Image: './images/speaker.png',
+		summary: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium possimus error tenetur eligendi sunt adipisci?',
+		details: 'Berkmann Professor of Enterpreunerial legal studies at Harvard Law School',
+	},
+
+];
 
 export default speakersInfo;
