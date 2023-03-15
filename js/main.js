b6eb2170363b1cf8da699d32ea9246b5c25df6e5
mutugi-c <mutugichris@gmail.com>
2023-03-15 16:04:53 +0100

    Adjust nav menu spacing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav__menu');
+const headline = document.querySelector('#headline');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  headline.classList.toggle('active');
 });
 
 navMenu.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  headline.classList.remove('active');
 });
